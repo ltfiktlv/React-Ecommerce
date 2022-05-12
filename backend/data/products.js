65011ffb3b1a1681ed26f25dc24bf3d3d451508d
unknown <lutfiikatlav@hotmail.com>
2022-05-12 23:45:40 +0300

    started to working on back end

---
+++ backend/data/products.js
@@ -0,0 +1,55 @@
+const products = [
+  {
+    _id: "1",
+    name: "Caffè Latte",
+    image: "/images/Caffe_Latte.jpg",
+    description:
+      "Balanced blend of rich espresso, steamed velvety milk and milk foam",
+    brand: "Savor",
+    category: "Beverages",
+    price: 35.45,
+    countInStock: 20,
+    rating: 4,
+    numReviews: 5,
+  },
+  {
+    _id: "2",
+    name: "Filter Coffee",
+    image: "/images/Filter_Coffee.jpg",
+    description: "(Filter Coffee) Brewed, balanced, dense filter coffee",
+    brand: "Savor",
+    category: "Beverages",
+    price: 15.55,
+    countInStock: 50,
+    rating: 3.5,
+    numReviews: 4,
+  },
+  {
+    _id: "3",
+    name: "Turkish Coffee",
+    image: "/images/Turkish_Coffee.jpg",
+    description:
+      "Traditional Turkish Coffee interpreted with Veranda Blend bean coffee",
+    brand: "Savor",
+    category: "Beverages",
+    price: 12.75,
+    countInStock: 45,
+    rating: 4.2,
+    numReviews: 10,
+  },
+  {
+    _id: "4",
+    name: "White Hot Chocolate",
+    image: "/images/White_Hot_Chocolate.jpg",
+    description:
+      "(White Hot Chocolate) Sweet flavor that emerges when steamed milk meets white chocolate sauce",
+    brand: "Savor",
+    category: "Beverages",
+    price: 38.75,
+    countInStock: 0,
+    rating: 4.8,
+    numReviews: 7,
+  },
+];
+
+export default products;
